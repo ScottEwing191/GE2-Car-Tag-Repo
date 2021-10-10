a950547f431a005956ab2904d8a3ccfa601d6cf4
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-10-10 19:30:47 +0100

    Update PlayerCollision.cs

--- Assets/Scripts/Player/PlayerCollision.cs
+++ Assets/Scripts/Player/PlayerCollision.cs
@@ -10,22 +10,21 @@ namespace CarTag.Player {
         }
 
         private void OnCollisionEnter(Collision collision) {
-            print("Collision");
+            //print("Collision");
             if (player.PlayerRoll != PlayerRoleEnum.Chaser) {
-                print(transform.parent.name + " : This car is not a Chaser");
+                //print(transform.parent.name + " : This car is not a Chaser");
                 return; }    //this car must be a chaser to continue
 
             if (!collision.transform.CompareTag("Player")) {
-                print(transform.parent.name + " : Other Object is not a Player");
+                //print(transform.parent.name + " : Other Object is not a Player");
                 return; }      // other object must be a car to continue
 
             if (!IsCollidingWithRunner(collision)) {
-                print(transform.parent.name + " : Colliding with other Chaser");
+                //print(transform.parent.name + " : Colliding with other Chaser");
                 return; }              // Must be colliding with the runner and not another chaser to continue
 
-            //if (GameManager.Instance.CheckpointManager.Checkpoints.Count != 0) { return; }  // Has the other car been trhough the required checkpoints
             if (GameManager.Instance.CheckpointManager.CheckpointQueues[player.PlayerListIndex].Count != 0) {
-                print(transform.parent.name+ " : Checkpoint Missed");
+                //print(transform.parent.name+ " : Checkpoint Missed");
                 return; }  // Has this car been through the required checkpoints
 
 
