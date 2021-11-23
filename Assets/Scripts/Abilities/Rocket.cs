60be4f57a1bec4390d55446e705047ff0ce181f6
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-11-23 19:28:14 +0000

    Pasted in Unity Fire Particles But not made any other changes

--- Assets/Scripts/Abilities/Rocket.cs
+++ Assets/Scripts/Abilities/Rocket.cs
@@ -2,32 +2,65 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CarTag.Abilities
-{
-    public class Rocket : MonoBehaviour
-    {
-        [SerializeField] private float explosionForce = 100;
-        [SerializeField] private float explosionRadius = 5;
-        
-        private void OnTriggerEnter(Collider other) {
-                print("Rocket Trigger Hit");
+namespace CarTag.Abilities {
+    public class Rocket : MonoBehaviour {
+        [SerializeField] private float explosionForce = 80000;
+        [SerializeField] private float explosionRadius = 3;
+        [SerializeField] private float fuseTime = 3;
+        [SerializeField] private bool useFuse = false;
+        [SerializeField] float rocketSpeed = 5;
 
-            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
-            List<Rigidbody> rigidBodies = new List<Rigidbody>();
+        Rigidbody thisRigidbody;
+        private float timeLeftTillExplode;
 
-            foreach (var c in colliders) {
-                Rigidbody rb = c.GetComponent<Rigidbody>();
-                if (rb != null) {
-                    rigidBodies.Add(rb);
-                    rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+        private void Start() {
+            thisRigidbody = GetComponent<Rigidbody>();
+            timeLeftTillExplode = fuseTime;
+            Move(rocketSpeed, transform.rotation);
+
+        }
+        private void Update() {
+            if (useFuse) {
+                timeLeftTillExplode -= Time.deltaTime;
+                if (timeLeftTillExplode <= 0) {
+                    Explode();
                 }
             }
-
-
-
-            Rigidbody body = other.GetComponentInParent<Rigidbody>();
-            body.AddExplosionForce(explosionForce, transform.position, explosionRadius);
         }
-        
+        private void OnTriggerEnter(Collider other) {
+            print("Rocket Trigger Hit");
+            Explode();
+
+        }
+
+        public void Move(float speed, Quaternion spawnRotation) {
+            transform.rotation = spawnRotation;
+            this.thisRigidbody.AddForce(transform.forward * speed, ForceMode.VelocityChange);
+        }
+
+        private void Explode() {
+            Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+            //List<Rigidbody> rigidBodies = new List<Rigidbody>();
+
+            foreach (var c in colliders) {
+                if (c.CompareTag("CollidesWithRocket")) {
+                    Rigidbody rb = c.GetComponent<Rigidbody>();
+                    if (rb == null) {
+                        rb = c.GetComponentInParent<Rigidbody>();
+                    }
+                    if (rb != null) {
+                        rb.AddExplosionForce(explosionForce, transform.position, explosionRadius);
+                    }
+                    else {
+                        Debug.Log("Rocket could not find a Rigidbody to explode on an object tagged with 'Collides With Rocket' ");
+                    }
+                }
+            }
+            Destroy(gameObject);
+        }
+
+        private void OnDrawGizmosSelected() {
+                Gizmos.DrawWireSphere(transform.position, explosionRadius);
+            }
+        }
     }
-}
