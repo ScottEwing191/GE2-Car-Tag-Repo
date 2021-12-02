dc0c146d33968b454e86861010728a7cffbf5b66
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-12-02 02:13:18 +0000

    Update FollowCar.cs

--- Assets/Scripts/Road Generation/FollowCar.cs
+++ Assets/Scripts/Road Generation/FollowCar.cs
@@ -12,6 +12,10 @@ namespace CarTag.Road
 
         private void Awake() {
             offset = transform.position - targetCar.position;
+            if (transform.rotation.eulerAngles.y != 0 || targetCar.rotation.eulerAngles.y != 0) {
+                Debug.Log("Either the target Car's or the game object the follow car script is attached to has a y rotation that is not 0. \n " +
+                "This has cause problems in the past");
+            }
         }
 
         private void LateUpdate() {
