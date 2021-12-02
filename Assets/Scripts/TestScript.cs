0158b74a824dbb033f8229f03526b8799e340a8f
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2021-12-02 02:53:24 +0000

    Test Car Swap Script Added

--- Assets/Scripts/TestScript.cs
+++ Assets/Scripts/TestScript.cs
@@ -5,14 +5,44 @@ namespace CarTag
 {
     public class TestScript : MonoBehaviour
     {
-        void Update() {
-            if (UnityEngine.Input.GetMouseButtonDown(0)) {
-                Ray ray = Camera.main.ScreenPointToRay(UnityEngine.Input.mousePosition);
-                RaycastHit hit;
+        public GameObject runner;
+        public GameObject chaser;
+        public Rigidbody runnerRB;
+        public Rigidbody chaserRB;
 
-                if (Physics.Raycast(ray, out hit, 100)) {
-                    Debug.Log(hit.transform.gameObject.name);
+        Vector3 currentVelocity;
+        Vector3 currentAngularVelocity;
+
+
+        void Update() {
+            if (UnityEngine.Input.GetKeyDown(KeyCode.T)) {
+                if (runner.activeInHierarchy) {
+                    chaser.transform.position = runner.transform.position;
+                    chaser.transform.rotation = runner.transform.rotation;
+
+                    currentVelocity = runnerRB.velocity;
+                    currentAngularVelocity = runnerRB.angularVelocity;
+
+                    runner.SetActive(false);
+                    chaser.SetActive(true);
+                    
+                    chaserRB.AddForce(currentVelocity, ForceMode.VelocityChange);
+                    chaserRB.AddTorque(currentAngularVelocity, ForceMode.VelocityChange);
+
                 }
+                else if (!runner.activeInHierarchy) {
+                    runner.transform.position = chaser.transform.position;
+                    runner.transform.rotation = chaser.transform.rotation;
+
+                    currentVelocity = chaserRB.velocity;
+                    currentAngularVelocity = chaserRB.angularVelocity;
+
+                    runner.SetActive(true);
+                    chaser.SetActive(false);
+
+                    runnerRB.AddForce(currentVelocity, ForceMode.VelocityChange);
+                    runnerRB.AddTorque(currentAngularVelocity, ForceMode.VelocityChange);
+                }  
             }
         }
     }
