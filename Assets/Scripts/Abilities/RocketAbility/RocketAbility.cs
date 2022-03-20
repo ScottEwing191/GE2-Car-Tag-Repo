98c65d38d4c39f1946968d578483faca65f65b76
ScottEwing191 <73129765+ScottEwing191@users.noreply.github.com>
2022-03-20 01:24:00 +0000

    Chenged Rocket power
    Started Rocket aim line
    Broke Runner Camera

--- Assets/Scripts/Abilities/RocketAbility/RocketAbility.cs
+++ Assets/Scripts/Abilities/RocketAbility/RocketAbility.cs
@@ -9,28 +9,38 @@ namespace CarTag.Abilities {
         //[SerializeField] private int runnerMaxUses = 3;
         //[SerializeField] private int chaserMaxUses = 3;
         //--Private
-        Transform spawnTransform;
-        Rocket currentRocket;
+        Transform _spawnTransform;
+        Rocket _currentRocket;
+        private LineRenderer _lineRenderer;
 
         public Transform SpawnTransform {
-            get { return spawnTransform; }
-            set { spawnTransform = value; }
+            get { return _spawnTransform; }
+            set { _spawnTransform = value; }
         }
 
         protected override void Awake() {
             base.Awake();
-            spawnTransform = runnerSpawnTransform;      //-- the runner and chaser will both spawn the rocket at the same transform 
+            _spawnTransform = runnerSpawnTransform;      //-- the runner and chaser will both spawn the rocket at the same transform 
+            _lineRenderer = GetComponentInChildren<LineRenderer>();
         }
         public override void RoleStartSetup(bool isRunner) {
             usesLeft = chaserMaxUses;
-            if (isRunner) {
+            /*if (isRunner) {
                 usesLeft = runnerMaxUses;
-            }
+            }*/
             // Destroy current rocket if one exists
-            if (currentRocket != null) {
-                Destroy(currentRocket.gameObject);
+            if (_currentRocket != null) {
+                Destroy(_currentRocket.gameObject);
             }
+            //--Set the rocket aim Line Renderer's Parent
+            SetLineRendererParent();
+            base.RoleStartSetup(isRunner);
         }
+
+        private void SetLineRendererParent() {
+            //_lineRenderer.transform.parent = playerAbilityController.thisPlayer.RCC_CarController.transform;
+        }
+
         public override bool CanStartAbility() {
             if (usesLeft <= 0) {
                 return false;
@@ -42,13 +52,13 @@ namespace CarTag.Abilities {
             if (!CanStartAbility()) {
                 return;
             }
-            currentRocket = Instantiate(abilityPrefab, spawnTransform.position, spawnTransform.rotation).GetComponent<Rocket>();
+            _currentRocket = Instantiate(abilityPrefab, _spawnTransform.position, _spawnTransform.rotation).GetComponent<Rocket>();
             LayerMask collidableLayers = collisionCheckLayers;
             //float carSpeed = playerAbilityController.thisPlayer.CarController.CurrentSpeed;
             float carSpeed = playerAbilityController.thisPlayer.RCC_CarController.GetComponent<Rigidbody>().velocity.magnitude;
-            currentRocket.GetComponent<Rocket>().SetSpawner(playerAbilityController.thisPlayer);
-            currentRocket.PlayerWhoFired = playerAbilityController.thisPlayer;      // For GUR Telemetry
-            currentRocket.StartRocket(carSpeed);
+            _currentRocket.GetComponent<Rocket>().SetSpawner(playerAbilityController.thisPlayer);
+            _currentRocket.PlayerWhoFired = playerAbilityController.thisPlayer;      // For GUR Telemetry
+            _currentRocket.StartRocket(carSpeed);
             AbilityUsed();
         }
 
